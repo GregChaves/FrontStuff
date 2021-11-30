9b98a4585ea42ebb089e715f39b82751a60c2195
Gregorio Brito Chaves <gbritoch@everis.com>
2021-11-30 10:05:27 -0300

    Initial commit

---
+++ UploadAndSplitPdf/src/app/services/file-upload.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+  private baseUrl = 'http://localhost:8081/api/v1/pdf';
+
+  constructor(private http: HttpClient) { }
+
+  upload(file: File): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+
+    formData.append('file', file);
+
+    const req = new HttpRequest('POST', `${this.baseUrl}/upload`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+
+    return this.http.request(req);
+  }
+
+  download(fileName: string) {
+
+    return this.http.get(`${this.baseUrl}/download/`+fileName, {
+      responseType: 'arraybuffer'
+    });
+   
+  
+  }
+
+}
